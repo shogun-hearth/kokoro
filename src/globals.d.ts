c89efb7abd89142baa2ccb30a31071b1912852f8
Sondhayni <sondhayni@gmail.com>
2021-11-16 02:17:08 -0800

    configs + packages

---
+++ src/globals.d.ts
@@ -0,0 +1,27 @@
+declare type Color =
+  'basic100' |
+  'basic300' |
+  'basic500' |
+  'basic700' |
+  'basic900' |
+  'basic1100' |
+  'primary100' |
+  'primary300' |
+  'primary500' |
+  'primary700' |
+  'primary900' |
+  'success100' |
+  'success300' |
+  'success500' |
+  'success700' |
+  'success900' |
+  'warning100' |
+  'warning300' |
+  'warning500' |
+  'warning700' |
+  'warning900' |
+  'danger100' |
+  'danger300' |
+  'danger500' |
+  'danger700' |
+  'danger900';
