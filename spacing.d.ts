e607dd1774c98f11f9dffdd8f51394ab926f85f3
Automated Publisher <actions@users.noreply.github.com>
2021-12-08 07:44:39 +0000

    Automated publish: Wed Dec  8 07:44:39 UTC 2021 e369cfb091e45c7b3ce0280b2329e709a4ecc7ff

--- spacing.d.ts
+++ spacing.d.ts
@@ -1,7 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 08 Dec 2021 07:20:00 GMT
+ * Generated on Wed, 08 Dec 2021 07:44:39 GMT
  */
 
 export const Spacing8 : any;
-export const ColorBlue500 : string;
