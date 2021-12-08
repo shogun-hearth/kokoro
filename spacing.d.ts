098599b5e197afcc9dcad9ba989be40aaf9c4214
Automated Publisher <actions@users.noreply.github.com>
2021-12-08 06:41:03 +0000

    Automated publish: Wed Dec  8 06:41:03 UTC 2021 5fe21e747132248e26e668f0e5b93116db20d4ea

--- spacing.d.ts
+++ spacing.d.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 08 Dec 2021 06:31:06 GMT
+ * Generated on Wed, 08 Dec 2021 06:41:02 GMT
  */
 
 export const Spacing8 : any;
--- spacing.js
+++ spacing.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 08 Dec 2021 06:31:06 GMT
+ * Generated on Wed, 08 Dec 2021 06:41:02 GMT
  */
 
 export const Spacing8 = {"top":8,"bottom":8,"left":8,"right":8};
